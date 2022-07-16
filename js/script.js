7162d4f52cabf3a5e73e4423ae33471843691e5c
Nikita Rodyukov <nikitarodwork@gmail.com>
2022-07-16 22:37:37 +0300

    swiper fixes

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
     // breakpoint where swiper will be destroyed
     // and switches to a dual-column layout
     const breakpoint = window.matchMedia( '(min-width:768px)' );
+    
   
     // keep track of swiper instances to destroy later
     let mySwiper;
@@ -21,25 +22,15 @@
         // clean up old instances and inline styles when available
         if ( mySwiper !== undefined ) {
             mySwiper.destroy( true, true );
-            
-            const repairBlock = document.querySelector('.repair-block__items');
-            const itemsWrapper = repairBlock.querySelector('.items__wrapper');
-            repairBlock.classList.remove('swiper-container');
-            itemsWrapper.classList.remove('swiper-wrapper');
-            const items = itemsWrapper.querySelectorAll('.items__block');
-            for (let i = 0; i < items.length; i++){
-                items[i].classList.remove('swiper-slide');
-            };
         }
         // or/and do nothing
         return;
   
         // else if a small viewport and single column layout needed
         } else if ( breakpoint.matches === false ) {
-  
+            
           // fire small viewport version of swiper
-          return enableSwiper();
-  
+          return enableSwiper();  
         }
   
     };
